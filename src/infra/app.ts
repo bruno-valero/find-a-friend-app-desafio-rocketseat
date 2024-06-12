8930978b9fe0eb63e1ca40caec465654f49999e0
Bruno Fernandes <brunofvn6@gmail.com.com>
2024-06-12 09:15:16 -0300

    implements global error handling

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -4,6 +4,9 @@ import { resolve } from 'node:path'
 import { readFileSync } from 'node:fs'
 import { orgRoutes } from './http/routes/org-routes'
 import { petRoutes } from './http/routes/pet-routes'
+import { env } from '@/env/env'
+import { ZodError } from 'zod'
+import { DataValidationError } from '@/core/errors/errors/data-validation-error'
 
 export const app = fastify()
 
@@ -23,3 +26,20 @@ app.register(fastifyJwt, {
 
 app.register(orgRoutes)
 app.register(petRoutes, { prefix: '/pets' })
+
+app.setErrorHandler((err, _req, res) => {
+  if (env.NODE_ENV !== 'prod') {
+    console.error(err)
+  }
+  if (err instanceof ZodError) {
+    return res
+      .status(400)
+      .send({ message: 'Validation error', issues: err.format() })
+  }
+  if (err instanceof DataValidationError) {
+    return res
+      .status(400)
+      .send({ message: 'Validation error', issues: err.message })
+  }
+  return res.status(500).send({ message: 'Internal server error.' })
+})
